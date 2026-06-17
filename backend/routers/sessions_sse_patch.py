457e17ebc5b5986344a350c2265828ab55beabdb
ikhsanadityaaa <ikhsanadityainfo@gmail.com>
2026-06-17 20:47:09 +0700

    Delete backend directory

--- backend/routers/sessions_sse_patch.py
+++
@@ -1,7 +0,0 @@
-"""
-Catatan historis: dukungan SSE dengan token query param sudah dipindahkan
-langsung ke routers/sessions.py.
-
-File ini dibiarkan sebagai penanda migrasi agar import package routers tidak
-gagal saat alat validasi memuat semua file Python.
-"""
